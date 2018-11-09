408fda762a2c4c0ab480fc3913288b319522a7de
Martim <martimfj@al.insper.edu.br>
2018-11-09 09:24:19 -0200

    WIP

--- README.md
+++ README.md
@@ -1,18 +1,44 @@
 # Openstack Shade App
 Esse projeto é uma reprodução do tutorial de Getting Started, que pode ser encontrado no site do openstack [site do Openstack.](https://developer.openstack.org/firstapp-shade/getting_started.html) Que tem como objetivo ensinar a programar utilizando componentes do Openstack.
 
-## Choose your OpenStack SDK
+## Escolha de um OpenStack SDK
 Escolhi o Shade, que utiliza Python, uma linguagem que tenho mais familiaridade e noções avançadas. 
 
 * [Shade](https://docs.openstack.org/infra/shade/): A Python-based library developed by OpenStack Infra team. Use it to operate multiple OpenStack clouds.	
 
-## What you need
+## Requerimentos
 Para conseguir as credenciais, foi preciso acessar o dashboard Horizon, acessar a aba *API Access* e baixar o RC File.
 
-## How you interact with OpenStack
+## Credenciais OpenStack
 Para adicionar as credenciais juntamente com as existentes:
 * `nano ~cloud/.config/openstack/clouds.yaml`
+```
+clouds:
+  openstack:
+    auth:
+      auth_url: http://192.168.1.33:5000/v3
+      username: "Hugo"
+      project_id: e6b2220a057049c4b6fd78444dbd7d71
+      project_name: "HugoSuperProj"
+      user_domain_name: "admin_domain"
+      password: "123456"
+    region_name: "RegionOne"
+    interface: "public"
+    identity_api_version: 3
+  martim_openstack:
+    auth:
+      auth_url: http://192.168.1.33:5000/v3
+      username: "martim"
+      project_id: f0263df0ae984f50b74204a1f920fc0f
+      project_name: "MartimProj"
+      user_domain_name: "admin_domain"
+      password: "123456"
+    region_name: "RegionOne"
+    interface: "public"
+    identity_api_version: 3
+```
 
-## Launch an instance
+## Launch de uma instância
+
 
 
--- teste.py
+++ teste.py
@@ -3,14 +3,60 @@ from shade import *
 simple_logging(debug=True)
 conn = openstack_cloud(cloud='martim_openstack')
 
-images = conn.list_images()
-flavors =  conn.list_flavors()
+def get_images():
+    image_list = []
+    for image in conn.list_images():
+        image_list.append((image['name'], image['id']))
+    return image_list
 
-image_id = '5d215499-f728-4cfa-b21c-ebdb34a9f4fe'
-image = conn.get_image(image_id)
+def get_flavors():
+    flavor_list = []
+    for flavor in conn.list_flavors():
+        flavor_list.append((flavor['id'], flavor['name']))
+    return flavor_list
 
-flavor_id = '9dd13582-b003-4c4a-813f-92c71890f4fe'
-flavor = conn.get_flavor(flavor_id)
+def get_instances():
+    instance_list = []
+    for instance in conn.list_servers():
+        instance_list.append((instance_list['id'], instance_list['name']))
+    return instance_list
+
+def get_keypairs():
+    keypair_list = []
+    for keypair in conn.list_keypairs():
+        keypair_list.append()
+
+# keypair_name = 'demokey'
+# pub_key_file = '/home/cloud/.ssh/id_rsa.pub'
+
+# if conn.search_keypairs(keypair_name):
+#     print('Keypair already exists. Skipping import.')
+# else:
+#     print('Adding keypair...')
+#     conn.create_keypair(keypair_name, open(pub_key_file, 'r').read().strip())
+
+# for keypair in conn.list_keypairs():
+#     print(keypair)
+
+
+# flavor_id = 
+# image_id = 
+
+
+# image = conn.get_image(image_id)
+# flavor = conn.get_flavor(flavor_id)
+
+
+# name=instance_name,
+# image=image_id,
+# flavor=flavor_id,
+
+# key_name=keypair_name,
+# security_groups=[sec_group_name],
+# userdata=ex_userdata,
+# network=network_id)
+
+
 '''
 print(image, flavor)
 
@@ -21,10 +67,6 @@ testing_instance = conn.create_server(wait=True, auto_ip=True,
     flavor=flavor_id,
 	network='b5326950-875d-450b-bbe9-eac01428eff7')
 print(testing_instance)
-
-instances = conn.list_servers()
-for instance in instances:
-    print(instance)
 
 conn.delete_server(name_or_id='215b225f-7ed9-46eb-9d83-d76434a575bd')
 conn.delete_server(name_or_id='60afee71-186f-43c2-ac20-fce0fb5728fd')
@@ -44,27 +86,27 @@ for keypair in conn.list_keypairs():
     print(keypair)
 '''
 
-sec_group_name = 'all-in-one'
-conn.search_security_groups(sec_group_name)
+# sec_group_name = 'all-in-one'
+# conn.search_security_groups(sec_group_name)
 
-ex_userdata = '''#!/usr/bin/env bash
+# ex_userdata = '''#!/usr/bin/env bash
 
-curl -L -s https://git.openstack.org/cgit/openstack/faafo/plain/contrib/install.sh | bash -s -- \
--i faafo -i messaging -r api -r worker -r demo
-'''
-network_id ='b5326950-875d-450b-bbe9-eac01428eff7'
-keypair_name = 'demokey'
-instance_name = 'all-in-one-final'
+# curl -L -s https://git.openstack.org/cgit/openstack/faafo/plain/contrib/install.sh | bash -s -- \
+# -i faafo -i messaging -r api -r worker -r demo
+# '''
+# network_id ='b5326950-875d-450b-bbe9-eac01428eff7'
+# keypair_name = 'demokey'
+# instance_name = 'all-in-one-final'
 
-testing_instance = conn.create_server(wait=True, auto_ip=False,
-    name=instance_name,
-    image=image_id,
-    flavor=flavor_id,
-    key_name=keypair_name,
-    security_groups=[sec_group_name],
-    userdata=ex_userdata,
-	network=network_id)
+# testing_instance = conn.create_server(wait=True, auto_ip=False,
+#     name=instance_name,
+#     image=image_id,
+#     flavor=flavor_id,
+#     key_name=keypair_name,
+#     security_groups=[sec_group_name],
+#     userdata=ex_userdata,
+# 	network=network_id)
 
-f_ip = conn.available_floating_ip()
-conn.add_ip_list(testing_instance, [f_ip['floating_ip_address']])
-print('The Fractals app will be deployed to http://%s' % f_ip['floating_ip_address'] )
+# f_ip = conn.available_floating_ip()
+# conn.add_ip_list(testing_instance, [f_ip['floating_ip_address']])
+# print('The Fractals app will be deployed to http://%s' % f_ip['floating_ip_address'] )
